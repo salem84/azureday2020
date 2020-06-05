e8a07623ccc27e9919e295b0607a66b86cffb17c
Giorgio Lasala <giorgio.lasala@eng.it>
2020-06-06 00:49:03 +0200

    aggiornato test

--- tests/performance/get_events.js
+++ tests/performance/get_events.js
@@ -1,5 +1,34 @@
 import { check, group, sleep } from "k6";
 import http from "k6/http";
+import { Counter, Rate, Trend } from "k6/metrics";
+
+
+// Options
+export let options = {
+    // stages: [
+    //     { target: 100, duration: "30s" },
+    //     { target: 100, duration: "120s" },
+    //     { target: 120, duration: "60s" },
+    //     { target: 0, duration: "30s" }
+    // ],
+    stages: [
+        { target: 100, duration: "3s" },
+        { target: 100, duration: "12s" },
+        { target: 120, duration: "6s" },
+        { target: 0, duration: "3s" }
+    ],
+    thresholds: {
+        "http_req_duration": ["p(95)<500"],
+        "http_req_duration": ["p(99)<10"],
+        "http_req_duration{staticAsset:yes}": ["p(95)<100"],
+        "check_failure_rate": ["rate<0.3"]
+    }
+};
+
+// Custom metrics
+var successfulLogins = new Counter("successful_logins");
+var checkFailureRate = new Rate("check_failure_rate");
+var timeToFirstByte = new Trend("time_to_first_byte", true);
 
 // User scenario
 export default function() {
@@ -7,8 +36,15 @@ export default function() {
         let res = http.get("https://historicevents.azurewebsites.net/api/events");
 
         check(res, {
-            "is status 200": (r) => r.status === 200
+            "is status 200": (r) => r.status === 200,
+            "body is greater than 1KB": (r) => r.body.length > 1024,
         });
+
+        // Record check failures
+        checkFailureRate.add(!checkRes);
+
+        // Record time to first byte and tag it with the URL 
+        timeToFirstByte.add(res.timings.waiting, { url: res.url });
 
         sleep(5);
     });
