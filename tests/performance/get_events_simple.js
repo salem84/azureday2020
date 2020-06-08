6453727f7fd747e473078bdb74b1efd124bb1725
Giorgio Lasala <giorgio.lasala@eng.it>
2020-06-08 20:47:03 +0200

    performance test simple

---
+++ tests/performance/get_events_simple.js
@@ -0,0 +1,44 @@
+import { check, group, sleep } from "k6";
+import http from "k6/http";
+import { Counter, Rate, Trend } from "k6/metrics";
+
+
+// Options
+export let options = {
+    stages: [
+        { target: 10, duration: "3s" },
+        { target: 8, duration: "12s" },
+        { target: 12, duration: "6s" },
+        { target: 0, duration: "3s" }
+    ],
+    thresholds: {
+        "http_req_duration": ["p(95)<500"],
+        "http_req_duration": ["p(99)<10"],
+        "check_failure_rate": ["rate<0.3"]
+    }
+};
+
+// Custom metrics
+var successfulLogins = new Counter("successful_logins");
+var checkFailureRate = new Rate("check_failure_rate");
+var timeToFirstByte = new Trend("time_to_first_byte", true);
+
+// User scenario
+export default function() {
+    group("Get Events", function() {
+        let res = http.get("https://historicevents.azurewebsites.net/api/events");
+
+        let checkRes = check(res, {
+            "is status 200": (r) => r.status === 200,
+            "body is greater than 200 bytes": (r) => r.body.length > 200,
+        });
+
+        // Record check failures
+        checkFailureRate.add(!checkRes);
+
+        // Record time to first byte and tag it with the URL 
+        timeToFirstByte.add(res.timings.waiting, { url: res.url });
+
+        sleep(5);
+    });
+}
